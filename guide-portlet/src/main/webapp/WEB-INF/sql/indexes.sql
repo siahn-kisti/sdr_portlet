0d3ec116d85d7c7271498b091604321e2df834ff
siahn-kisti <siahn@kisti.re.kr>
2018-12-03 11:39:07 +0900

    remove -r SDR_guide-portlet

--- guide-portlet/src/main/webapp/WEB-INF/sql/indexes.sql
+++
@@ -1,7 +0,0 @@
-create index IX_D8B56FB0 on SDR_Guide (uuid_);
-create index IX_427AE678 on SDR_Guide (uuid_, companyId);
-create unique index IX_8FFAAAFA on SDR_Guide (uuid_, groupId);
-
-create index IX_A0BADB1F on test_Guide (uuid_);
-create index IX_4B7B4FA9 on test_Guide (uuid_, companyId);
-create unique index IX_9BB5AC6B on test_Guide (uuid_, groupId);
